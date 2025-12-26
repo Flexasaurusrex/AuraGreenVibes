79236342ef8e984367b4faa510011a7a8c839a20
Flexasaurusrex <108902627+Flexasaurusrex@users.noreply.github.com>
2025-12-26 02:02:28 -0800

    Update DinoGame.js

--- app/DinoGame.js
+++ app/DinoGame.js
@@ -1,7 +1,6 @@
 'use client';
 
 import { useState, useEffect, useCallback } from 'react';
-import { motion, AnimatePresence } from 'framer-motion';
 import DinoGame from './DinoGame';
 
 export default function Portfolio() {
